cb3e3fbf2ef6629f0d9446348da97738c4f1fcc1
Carolina Della Nina <carolirn@gmail.com>
2020-08-20 09:40:13 -0300

    ultima versao carol

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Carrinho from "./components/Carrinho.js";
+import { CardFiltro } from "./components/Filtro";
+import TabelaProdutos from "./components/TabelaProdutos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DivPainel = styled.div`
+  display: flex;
+  max-width: 80%;
+  flex-wrap: wrap;
+`;
+
+const DivHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 2em;
+  margin-top: 1em;
+  padding: 1em 3em;
+`;
+
+class App extends React.Component {
+  state = {
+    arrayProdutos: [
+      {
+        nomeProduto: "Teste1",
+        valorProduto: 10,
+      },
+      {
+        nomeProduto: "Teste2",
+        valorProduto: 20,
+      },
+      {
+        nomeProduto: "Teste1",
+        valorProduto: 30,
+      },
+      {
+        nomeProduto: "Teste1",
+        valorProduto: 40,
+      },
+      {
+        nomeProduto: "Teste1",
+        valorProduto: 40,
+      },
+    ],
+  };
+
+  render() {
+    const quantidadeProdutos = this.state.arrayProdutos.length;
+
+    const produtosRender = this.state.arrayProdutos.map((produto) => {
+      return (
+        <TabelaProdutos
+          nomeDoProduto={produto.nomeProduto}
+          valorProduto={produto.valorProduto}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <CardFiltro />
+        <DivHeader>
+          <p>Quantidade de Produtos: {quantidadeProdutos} </p>
+          <select>
+            <option value="Crescente">Crescente</option>
+            <option value="Decrescente">Decrescente</option>
+          </select>
+        </DivHeader>
+        <DivPainel>{produtosRender}</DivPainel>
+      </div>
+    );
+  }
 }
 
 export default App;
